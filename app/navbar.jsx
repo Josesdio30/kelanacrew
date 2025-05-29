088b3dc252fb7ea5bbd550b2905442ab3f25cfa9
Baracky <135500505+Baracky@users.noreply.github.com>
2025-05-29 11:41:01 +0700

    Update navbar.jsx

--- app/navbar.jsx
+++ app/navbar.jsx
@@ -1,1 +1,20 @@
+import React from 'react'
+import Link from 'next/link'
+import styles from './navbar.module.css'
+import Image from 'next/image'
 
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+        <h2 className={styles.logo}>KELANA CREW</h2>
+        <Link href="/" className={styles.link}>Home</Link>
+        <Link href="/about" className={styles.link}>About</Link>
+        <Link href="/contact" className={styles.link}>Contact</Link>
+        <Link href="/trip_type" className={styles.link}>Activity</Link>
+        <Link href="/event" className={styles.link}>Event</Link>
+        <Link href="/destination" className={styles.link}>Destination</Link>
+    </div>
+  )
+}
+
+export default Navbar
